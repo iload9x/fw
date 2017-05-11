3e089e55d6ca90227e605e6eddadcaf5654c0250
iload9x <wapa2pro@gmail.com>
2017-05-12 00:09:06 +0700

    up to herok222u

--- app/libraries/DatabaseLib.php
+++ app/libraries/DatabaseLib.php
@@ -5,10 +5,10 @@ class DatabaseLib
 {
 	function __construct() {
 		$this->config = array(
-			'hostname' 	=> '127.0.0.1:3306',
-			'username'	=> 'root',
-			'password' 	=> '',
-			'dbname'	=> 'ngoancms'
+			'hostname' 	=> 'us-cdbr-iron-east-03.cleardb.net',
+			'username'	=> 'bba4bd358d9dae',
+			'password' 	=> '88ca662b',
+			'dbname'	=> 'heroku_cc3ffde785b8c00'
 		);
 	}
 }
